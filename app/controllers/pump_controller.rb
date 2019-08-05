c69727226c7e717c6ecd3a97b1c753ab7f74a086
martynj2001 <martynj2010@icloud.com>
2019-08-05 20:18:41 +0100

    Changed GET response to ||0|| & ||1||

--- app/controllers/pump_controller.rb
+++ app/controllers/pump_controller.rb
@@ -1,6 +1,6 @@
 class PumpController < ApplicationController
 
-  @@pumpStatus = 'OFF'
+  @@pumpStatus = '||0||'
   
   def pump_status
     render plain: @@pumpStatus
@@ -8,10 +8,10 @@ class PumpController < ApplicationController
 
   def set_pump_status
     @@pumpStatus = params[:status]
-    if @@pumpStatus == "ON"
-        flash[:notice] = "You have turned the Greenhouse watering system #{@@pumpStatus}"      
+    if @@pumpStatus == "||1||"
+        flash[:notice] = "You have turned the Greenhouse watering system ON"      
     else
-        flash[:alert] = "You have turned the Greenhouse watering system #{@@pumpStatus}" 
+        flash[:alert] = "You have turned the Greenhouse watering system OFF" 
     end
     redirect_to measures_path
   end
